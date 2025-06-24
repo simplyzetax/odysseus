1fcf70478b84e1d99d1ad138480cef2f066c029a
Zetax <ernzerhofffinn@gmail.com>
2025-06-24 09:20:44 +0200

    feat: enhance getDB function to utilize colo information for Durable Object cache

--- src/core/db/client.ts
+++ src/core/db/client.ts
@@ -2,10 +2,19 @@ import { CloudflareKVDrizzleCache } from "@utils/caches/drizzle-workers-kv-cache
 import { drizzle } from "drizzle-orm/postgres-js";
 import { Context } from "hono";
 import { CloudflareDurableObjectRPCDrizzleCache } from "../../utils/caches/drizzle-workers-do-cache";
+import { odysseus } from "@core/error";
 
 export const getDB = (c: Context<{ Bindings: Env }>) => {
+
+    const colo = String(c.req.raw.cf?.colo);
+    if (!colo) {
+        odysseus.internal.serverError.withMessage("No colo information available in request context").throwHttpException();
+    }
+
+    console.log(`Using colo: ${colo} for Durable Object cache`);
+
     // Get the Durable Object namespace from the Cloudflare environment
-    const durableObjectCache = new CloudflareDurableObjectRPCDrizzleCache(c.env.CACHE_DO, "drizzle-cache");
+    const durableObjectCache = new CloudflareDurableObjectRPCDrizzleCache(c.env.CACHE_DO, colo);
 
     return drizzle(c.env.DB.connectionString, {
         cache: durableObjectCache
