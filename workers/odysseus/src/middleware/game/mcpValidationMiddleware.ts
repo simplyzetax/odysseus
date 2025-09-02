8021ac3d5816eade333f5128f877f24c6077c364
Finn <ernzerhofffinn@icloud.com>
2025-09-02 14:23:51 +0200

    Update mcpValidationMiddleware.ts

--- workers/odysseus/src/middleware/game/mcpValidationMiddleware.ts
+++ workers/odysseus/src/middleware/game/mcpValidationMiddleware.ts
@@ -4,11 +4,6 @@ import { FortniteProfile } from '@utils/mcp/base-profile';
 import type { Context } from 'hono';
 import { createMiddleware } from 'hono/factory';
 
-/**
- * Middleware that parses and validates the revision number from the request
- * and adds profile revision information to the response so we don't have to handle
- * it manually in the routes.
- */
 export const mcpValidationMiddleware = createMiddleware(
 	async (c: Context<{ Bindings: Env } & { Variables: { profileType: ProfileType } }>, next) => {
 		const requestedProfileId = c.req.query('profileId');
